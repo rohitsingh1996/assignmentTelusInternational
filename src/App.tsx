df4a914cf4f3e5dedb2f8926002e4bc269ab1997
Rohit Singh <rohit.r.rajput007@gmail.com>
2022-01-26 01:26:36 +0530

    Added Static Ui component  for List

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import List from './components/List';
 
 function App() {
   return (
     <div>
-      
+      <List />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,28 @@
+import React  from "react";
+import classes from './List.module.css'
+
+const List: React.FC = (props) => {
+    return (
+        <table>
+            <caption>Employee Information</caption>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Nick Pettit</th>
+                    <td>nick@example.com</td>
+                    <td>
+                        <button className={classes.editBtn}>Edit</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
+  
+export default List;
